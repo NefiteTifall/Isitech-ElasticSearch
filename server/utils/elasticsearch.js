c2d0e6feaead7f2b3e505131bc2bb28abc6f4ac8
Anthony Vallenet <anthony.vallenet@geode.com>
2024-05-14 11:32:23 +0200

    Auto stash before merge of "main" and "origin/main"

---
+++ server/utils/elasticsearch.js
@@ -0,0 +1,13 @@
+import { Client } from '@elastic/elasticsearch';
+
+const ElasticClient = new Client({
+    cloud: {
+        id: process.env.ELASTIC_CLOUD_ID
+    },
+    auth: {
+        username: process.env.ELASTIC_USER,
+        password: process.env.ELASTIC_PASSWORD
+    }
+});
+
+export { ElasticClient };
