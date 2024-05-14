a468d1f52498ea44155a6dcbd7bd470a52b4c5b6
Anthony Vallenet <anthony.vallenet@geode.com>
2024-05-14 16:04:36 +0200

    🔍 Restructure search queries for better results
    - Refactored search queries to include fuzzy and wildcard searches for improved accuracy and flexibility. Removed redundant search endpoints.

--- server/api/candidates/index.get.js
+++ server/api/candidates/index.get.js
@@ -1,17 +1,41 @@
 import { ElasticClient } from '../../utils/elasticsearch';
 
 export default defineEventHandler(async (event) => {
+    const query = getQuery(event);
+    const searchQuery = query.query || '';
+
     try {
         const result = await ElasticClient.search({
             index: 'tweets',
             body: {
                 size: 0,
+                query: {
+                    bool: {
+                        should: [
+                            {
+                                fuzzy: {
+                                    label: {
+                                        value: searchQuery,
+                                        fuzziness: "AUTO"
+                                    }
+                                }
+                            },
+                            {
+                                wildcard: {
+                                    label: {
+                                        value: `*${searchQuery}*`
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                },
                 aggs: {
                     tweets_per_candidate: {
                         terms: {
                             field: 'label',
                             size: 100
-                        }
+                        },
                     }
                 }
             }
